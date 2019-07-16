733de7eaaff8116ba1235c04bc0a3149cad8f7ec
Frank Wu <340629194@qq.com>
2019-07-16 15:40:36 +0800

    report exception

--- executor/invoker.py
+++ executor/invoker.py
@@ -212,7 +212,8 @@ def vMWatcher():
 #                     undefine_with_snapshot(metadata_name)
         except:
             logger.error('Oops! ', exc_info=1)
-            report_failure(name, jsondict, GROUP_VM, VERSION_VM, PLURAL_VM)
+            info=sys.exc_info()
+            report_failure(name, jsondict, info[0], info[1], GROUP_VM, VERSION_VM, PLURAL_VM)
         
                 
 def vMDiskWatcher():
@@ -246,7 +247,8 @@ def vMDiskWatcher():
                         runCmd(cmd)   
         except:
             logger.error('Oops! ', exc_info=1)
-            report_failure(name, jsondict, GROUP_VM_DISK, VERSION_VM_DISK, PLURAL_VM_DISK)
+            info=sys.exc_info()
+            report_failure(name, jsondict, info[0], info[1], GROUP_VM_DISK, VERSION_VM_DISK, PLURAL_VM_DISK)
                 
                 
 def vMImageWatcher():
@@ -279,7 +281,8 @@ def vMImageWatcher():
                     undefine(metadata_name)
         except:
             logger.error('Oops! ', exc_info=1)
-            report_failure(name, jsondict, GROUP_VMI, VERSION_VMI, PLURAL_VMI)
+            info=sys.exc_info()
+            report_failure(name, jsondict, info[0], info[1], GROUP_VMI, VERSION_VMI, PLURAL_VMI)
         
 def vMSnapshotWatcher():
     watcher = watch.Watch()
@@ -310,9 +313,10 @@ def vMSnapshotWatcher():
                     cmd = unpackCmdFromJson(jsondict)
                     if cmd: 
                         runCmd(cmd)  
-        except:
+        except Exception, e:
             logger.error('Oops! ', exc_info=1)
-            report_failure(name, jsondict, GROUP_VM_SNAPSHOT, VERSION_VM_SNAPSHOT, PLURAL_VM_SNAPSHOT)
+            info=sys.exc_info()
+            report_failure(name, jsondict, info[0], info[1], GROUP_VM_SNAPSHOT, VERSION_VM_SNAPSHOT, PLURAL_VM_SNAPSHOT)
 
 def vMBlockDevWatcher():
     watcher = watch.Watch()
@@ -346,9 +350,10 @@ def vMBlockDevWatcher():
                         runCmd(cmd)   
         except:
             logger.error('Oops! ', exc_info=1)
-            report_failure(name, jsondict, GROUP_BLOCK_DEV_UIT, VERSION_BLOCK_DEV_UIT, PLURAL_BLOCK_DEV_UIT)
+            info=sys.exc_info()
+            report_failure(name, jsondict, info[0], info[1], GROUP_BLOCK_DEV_UIT, VERSION_BLOCK_DEV_UIT, PLURAL_BLOCK_DEV_UIT)
 
-def report_failure(name, jsondict, group=None, version=None, plural=None):
+def report_failure(name, jsondict, error_reason=None, error_message=None, group=None, version=None, plural=None):
     try:
         jsondict = client.CustomObjectsApi().get_namespaced_custom_object(group=group, 
                                                                           version=version, 
@@ -356,7 +361,7 @@ def report_failure(name, jsondict, group=None, version=None, plural=None):
                                                                           plural=plural, 
                                                                           name=name)
         jsondict = deleteLifecycleInJson(jsondict)
-        body = addExceptionMessage(jsondict)
+        body = addExceptionMessage(jsondict, error_reason, error_message)
         retv = client.CustomObjectsApi().replace_namespaced_custom_object(
             group=group, version=version, namespace='default', plural=plural, name=name, body=body)
         return retv

3384890fdb3d626942afafc34a8320a843edd7b1
syswu <wuheng@iscas.ac.cn>
2019-07-16 09:03:35 +0800

    fix typos

---
+++ controller/src/test/java/com/github/kube/controller/KubevirtControllerTest.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (2019, ) Institute of Software, Chinese Academy of Sciences
+ */
+package com.github.kube.controller;
+
+
+/**
+ * @author shizhonghao17@otcaix.iscas.ac.cn
+ * @author yangchen18@otcaix.iscas.ac.cn
+ * @author wuheng@otcaix.iscas.ac.cn
+ * @since Wed May 01 17:26:22 CST 2019
+ * 
+ * https://www.json2yaml.com/
+ * http://www.bejson.com/xml2json/
+ * 
+ * debug at runWatch method of io.fabric8.kubernetes.client.dsl.internal.WatchConnectionManager
+ **/
+public class KubevirtControllerTest {
+	
+	public static void main(String[] args) throws Exception {
+		KubevirtController scheduler = new KubevirtController("admin.conf");
+		scheduler.start();
+	}
+
+}

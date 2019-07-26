ca8144669cf7e00c774636ba2b80f4cb5dd59950
wuyuewen <suhupt_f@126.com>
2019-07-26 17:30:17 +0800

    fix bugs

--- executor/scripts/convert-vm-to-image.sh
+++ executor/scripts/convert-vm-to-image.sh
@@ -15,18 +15,18 @@ line1=`virsh list --all | awk '{ print $2 }' | grep -w $1 | wc -l`
 
 if [ $line1 -eq 1 ] 
 then
-    echo 'log info: vm exist...\n'
+    echo 'log info: vm exist...\n' >&1
 else
-    echo 'log error: vm not exist...\n'
+    echo 'log error: vm not exist...\n' >&2
     exit 1
 fi
 
 # check is exist the image or not
 if [ ! -f ${DEFAULT_IMAGE_PATH}${1}'.xml' ]
 then
-  echo "log info: image not exist, begin create image...\n"
+  echo "log info: image not exist, begin create image...\n" >&1
 else
-  echo "log error: image has exist, exit...\n"
+  echo "log error: image has exist, exit...\n" >&2
   exit 1
 fi
 
@@ -34,7 +34,7 @@ fi
 # check DEAFULT_PATH disk space support to create image or not
 DISK_FILE_PATH=`virsh domblklist $1 | awk 'NR==3 {print $2 }'`
 if [ ! -n "$DISK_FILE_PATH" ]; then
-  echo "log error: can't find vm disk file..."
+  echo "log error: can't find vm disk file..." >&2
   exit 1
 fi
 
@@ -44,11 +44,11 @@ if [ $DISK_SPACE -gt $NEED_SPACE ]
 then
     echo $NEED_SPACE
     echo $DISK_SPACE
-    echo "log info: space is enough..."
+    echo "log info: space is enough..." >&1
 else
     echo $NEED_SPACE
     echo $DISK_SPACE
-    echo "log error: space is not enough..."
+    echo "log error: space is not enough..." >&2
     exit 1
 fi
 
@@ -58,9 +58,9 @@ line2=`virsh list --all | grep -w $1  | grep 'shut' | wc -l`
 
 if [ $line2 -eq 1 ]
 then
-    echo 'log info: vm has shut down...'
+    echo 'log info: vm has shut down...' >&1
 else
-    echo 'log error: vm is running..., please shut down firstly...\n'
+    echo 'log error: vm is running..., please shut down firstly...\n' >&2
     exit 1
 #    virsh destroy $1
 #    if [ $? -ne 0 ]; then
@@ -75,10 +75,10 @@ fi
 virsh dumpxml $1 > ${DEFAULT_IMAGE_PATH}${1}.xml
 
 if [ $? -ne 0 ]; then
-    echo "log error: dump xml file fail...\n"
+    echo "log error: dump xml file fail...\n" >&2
     exit 1
 else
-    echo "log info: dump xml file successfully...\n"
+    echo "log info: dump xml file successfully...\n" >&1
 fi
 
 # step 2 copy the file to default path, and change the file path in xml
@@ -90,21 +90,21 @@ echo $IMAGE_PATH
 cp ${IMAGE_PATH} ${DEFAULT_IMAGE_PATH}${1}'.'${IMAGE_PATH#*.}
 
 if [ $? -ne 0 ]; then
-    echo "log error: copy image file fail...\n"
+    echo "log error: copy image file fail...\n" >&2
     # operation fial, roll back
     rm ${DEFAULT_IMAGE_PATH}${1}.xml
     exit 1
 else
     sed -i 's#'${IMAGE_PATH}'#'${DEFAULT_IMAGE_PATH}${1}'.'${IMAGE_PATH#*.}'#g' ${DEFAULT_IMAGE_PATH}${1}.xml
     if [ $? -ne 0 ]; then
-        echo "log error: change the image file path in xml file failed\n"
+        echo "log error: change the image file path in xml file failed\n" >&2
         # operation fial, roll back
         rm -f ${DEFAULT_IMAGE_PATH}${1}.xml ${DEFAULT_IMAGE_PATH}${1}'.'${IMAGE_PATH#*.}
         exit 1
     else
-        echo "log info: change the image file path in xml file successfully...\n"
+        echo "log info: change the image file path in xml file successfully...\n" >&1
     fi
-    echo "log info: copy image file successfully...\n"
+    echo "log info: copy image file successfully...\n" >&1
 
     # init image
 #    virt-sysprep -a ${DEFAULT_IMAGE_PATH}${IMAGE_PATH##*/}
@@ -130,11 +130,11 @@ if [ $? -ne 0 ]; then
     if [ $VM_IS_EXIST -eq 1 ]
     then
         # undefine fail, delete the files
-        echo "log error: undefine fail, delete the files...\n"
+        echo "log error: undefine fail, delete the files...\n" >&2
         rm -f ${DEFAULT_IMAGE_PATH}${1}.xml ${DEFAULT_IMAGE_PATH}${1}'.'${IMAGE_PATH#*.} ${DEFAULT_IMAGE_PATH}${1}'.path'
         exit 1
     else
-        echo 'log info: undefine vm successfully, delete the disk file manually...\n'
+        echo 'log info: undefine vm successfully, delete the disk file manually...\n' >&1
         rm -f ${IMAGE_PATH}
 #        if [ ! -d "${IMAGE_PATH}" ]; then
 #            echo 'log info: delete the disk file successfully...\n'
@@ -145,7 +145,5 @@ if [ $? -ne 0 ]; then
         exit 0
     fi
 else
-    echo "log info: undifine vm successfully...\n"
+    echo "log info: undifine vm successfully...\n" >&1
 fi
-
-

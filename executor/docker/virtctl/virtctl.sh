b79afe462c7fd63cf5334ba2f72ebcecd851d1a2
Frank Wu <suhupt_f@126.com>
2019-07-31 17:06:39 +0800

    stable v0.7

--- executor/docker/virtctl/virtctl.sh
+++ executor/docker/virtctl/virtctl.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 ##############################################################
 ##
 ##      Copyright (2019, ) Institute of Software
@@ -6,5 +7,11 @@
 ##
 ################################################################
 
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+cd $SHELL_FOLDER
+rm -rf dist/ build/ vmm.spec
+pyinstaller -F vmm.py -p ./
+chmod +x dist/vmm
+cp -f dist/vmm /usr/bin
 
 python virtctl_in_docker.py

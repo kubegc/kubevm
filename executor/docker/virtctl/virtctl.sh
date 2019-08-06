7194e010b72c9cce916fd95c5d15bd6d591482c7
wuyuewen <340629194@qq.com>
2019-08-06 10:00:10 +0800

    update docker

--- executor/docker/virtctl/virtctl.sh
+++ executor/docker/virtctl/virtctl.sh
@@ -7,11 +7,11 @@
 ##
 ################################################################
 
-SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
-cd $SHELL_FOLDER
-rm -rf dist/ build/ vmm.spec
-pyinstaller -F vmm.py -p ./
-chmod +x dist/vmm
-cp -f dist/vmm /usr/bin
+#SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+#cd $SHELL_FOLDER
+#rm -rf dist/ build/ vmm.spec
+#pyinstaller -F vmm.py -p ./
+#chmod +x dist/vmm
+#cp -f dist/vmm /usr/bin
 
 python virtctl_in_docker.py

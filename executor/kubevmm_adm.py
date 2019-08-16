b99a5d91a1f7c412392b43d6095db494ad34c0b2
Frank Wu <340629194@qq.com>
2019-08-16 19:50:23 +0800

    support ovs-bridge

--- executor/kubevmm_adm.py
+++ executor/kubevmm_adm.py
@@ -25,7 +25,11 @@ except:
     
 def check_version(ignore_warning=False):
     (virtctl_running_version, _) = runCmd('docker ps | grep \"bash virtctl.sh\" | awk \'{print $2}\' | awk -F\':\' \'{if(NF>1) print $2}\'')
+    if not virtctl_running_version:
+        (virtctl_running_version, _) = runCmd('docker ps | grep registry.cn-hangzhou.aliyuncs.com/cloudplus-lab/kubevirt-virtctl | awk \'{print $2}\' | awk -F\':\' \'{if(NF>1) print $2}\'')
     (virtlet_running_version, _) = runCmd('docker ps | grep \"bash virtlet.sh\" | awk \'{print $2}\' | awk -F\':\' \'{if(NF>1) print $2}\'')
+    if not virtlet_running_version:
+        (virtlet_running_version, _) = runCmd('docker ps | grep registry.cn-hangzhou.aliyuncs.com/cloudplus-lab/kubevirt-virtlet | awk \'{print $2}\' | awk -F\':\' \'{if(NF>1) print $2}\'')
     if not ignore_warning and virtctl_running_version and virtctl_running_version != VERSION or not ignore_warning and virtlet_running_version and virtlet_running_version != VERSION:
         print('warning: mismatch version detected!') 
         (style_1, style_2, style_3, style_4) = ('\033[1;42m', '\033[0m', '\033[1;42m', '\033[0m')
@@ -101,7 +105,11 @@ def restart(ignore_warning=False):
 def status(print_result=False, ignore_warning=False):
     (virtctl_running_version, virtlet_running_version) = check_version(ignore_warning=ignore_warning)
     (virtctl_container_id, virtctl_err) = runCmd("docker ps | grep \"bash virtctl.sh\" | awk \'NR==1{print $1}\'")
+    if not virtctl_container_id:
+        (virtctl_container_id, virtctl_err) = runCmd("docker ps | grep registry.cn-hangzhou.aliyuncs.com/cloudplus-lab/kubevirt-virtctl | awk \'NR==1{print $1}\'")
     (virtlet_container_id, virtlet_err) = runCmd("docker ps | grep \"bash virtlet.sh\" | awk \'NR==1{print $1}\'")
+    if not virtlet_container_id:
+        (virtlet_container_id, virtlet_err) = runCmd("docker ps | grep registry.cn-hangzhou.aliyuncs.com/cloudplus-lab/kubevirt-virtlet | awk \'NR==1{print $1}\'")
     if virtctl_err:
         print('warning: %s\n' % (virtctl_err))
     if virtlet_err:

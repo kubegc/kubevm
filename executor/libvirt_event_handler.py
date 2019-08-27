6da7e118110cb9f89850c01d0dcf02bab523bf9a
Frank Wu <340629194@qq.com>
2019-08-27 12:40:28 +0800

    fix bugs

--- executor/libvirt_event_handler.py
+++ executor/libvirt_event_handler.py
@@ -95,7 +95,16 @@ def myDomainEventHandler(conn, dom, *args, **kwargs):
                     vm_json = updateDomain(loads(vm_json))
                     jsondict = updateDomainStructureAndDeleteLifecycleInJson(jsondict, vm_json)
                     body = addPowerStatusMessage(jsondict, vm_power_state, 'The VM is %s' % vm_power_state)
-                    modifyVM(vm_name, body)
+                    i=3
+                    while (i>0):
+                        if i == 3:
+                            break;
+                        try:
+                            modifyVM(vm_name, body)
+                            time.sleep(0.5)
+                        except:
+                            i -= 1
+                            modifyVM(vm_name, body)
                     step1_done = True
                 except:
                     step1_done = False

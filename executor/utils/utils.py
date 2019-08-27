621a023ac1e4bd116eb61f58bce5981ec0a44685
wuyuewen <wuyuewen@otcaix.iscas.ac.cn>
2019-08-27 14:59:47 +0800

    support vm & pod

--- executor/utils/utils.py
+++ executor/utils/utils.py
@@ -206,7 +206,7 @@ def pid_exists(pid):
         return True
 
 def get_hostname_in_lower_case():
-    return socket.gethostname().lower()
+    return 'vm.%s' % socket.gethostname().lower()
 
 def normlize(s):
     return s[:1].upper() + s[1:]

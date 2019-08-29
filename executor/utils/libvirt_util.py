fda66588ba5e834e9362723ff26150ae623596a1
742362144 <742362144@qq.com>
2019-08-29 17:24:17 +0800

    add pool

--- executor/utils/libvirt_util.py
+++ executor/utils/libvirt_util.py
@@ -99,6 +99,8 @@ def _get_pool_info(pool_):
         pass
     lines = pool.XMLDesc()
     result = runCmdWithResult('virsh pool-info ' + pool_)
+    result['allocation'] = int(1024*1024*1024*result['allocation'])
+    result['available'] = int(1024 * 1024 * 1024 * result['available'])
     # del result['allocation']
     # del result['available']
     for line in lines.split():

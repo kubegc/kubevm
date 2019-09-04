7ddd9cb85268f63a18649c33eb7c6fa35f3d3c04
Frank Wu <340629194@qq.com>
2019-09-04 13:55:57 +0800

    support vmdi

--- executor/os_event_handler.py
+++ executor/os_event_handler.py
@@ -787,14 +787,14 @@ class VmdImageLibvirtXmlEventHandler(FileSystemEventHandler):
             logger.debug("directory moved from {0} to {1}".format(event.src_path,event.dest_path))
         else:
             logger.debug("file moved from {0} to {1}".format(event.src_path,event.dest_path))
-#             _,name = os.path.split(event.dest_path)
-#             file_type = os.path.splitext(name)[1]
-#             vmdi = os.path.splitext(os.path.splitext(name)[0])[0]
-#             if file_type == '.qcow2':
-#                 try:
-#                     myVmdImageLibvirtXmlEventHandler('Create', vmdi, self.pool, event.dest_path, self.group, self.version, self.plural)
-#                 except ApiException:
-#                     logger.error('Oops! ', exc_info=1)
+            _,name = os.path.split(event.dest_path)
+            file_type = os.path.splitext(name)[1]
+            vmdi = os.path.split(os.path.splitext(event.dest_path)[0])[1]
+            if file_type == '.qcow2':
+                try:
+                    myVmdImageLibvirtXmlEventHandler('Create', vmdi, self.pool, event.dest_path, self.group, self.version, self.plural)
+                except ApiException:
+                    logger.error('Oops! ', exc_info=1)
 
     def on_created(self, event):
         if event.is_directory:
@@ -803,7 +803,7 @@ class VmdImageLibvirtXmlEventHandler(FileSystemEventHandler):
             logger.debug("file created:{0}".format(event.src_path))
             _,name = os.path.split(event.src_path)
             file_type = os.path.splitext(name)[1]
-            vmdi = os.path.splitext(os.path.splitext(name)[0])[0]
+            vmdi = os.path.split(os.path.splitext(event.dest_path)[0])[1]
             if file_type == '.qcow2':
                 try:
                     myVmdImageLibvirtXmlEventHandler('Create', vmdi, self.pool, event.dest_path, self.group, self.version, self.plural)
@@ -817,7 +817,7 @@ class VmdImageLibvirtXmlEventHandler(FileSystemEventHandler):
             logger.debug("file deleted:{0}".format(event.src_path))
             _,name = os.path.split(event.src_path)
             file_type = os.path.splitext(name)[1]
-            vmdi = os.path.splitext(os.path.splitext(name)[0])[0]
+            vmdi = os.path.split(os.path.splitext(event.dest_path)[0])[1]
             if file_type == '.qcow2':
                 try:
                     myVmdImageLibvirtXmlEventHandler('Delete', vmdi, self.pool, event.src_path, self.group, self.version, self.plural)

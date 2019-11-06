c2dd3fb354c26887bd52647ef7d18721b6cf60b8
wuyuewen <340629194@qq.com>
2019-11-06 10:23:08 +0800

    fix bugs in create_vmdi

--- executor/vmm.py
+++ executor/vmm.py
@@ -26,7 +26,6 @@ from kubernetes.client.rest import ApiException
 from utils.libvirt_util import check_pool_content_type, refresh_pool, get_vol_info_by_qemu, get_volume_xml, get_pool_path, is_volume_in_use, is_volume_exists, get_volume_current_path, vm_state, is_vm_exists, is_vm_active, get_boot_disk_path, get_xml, undefine_with_snapshot, undefine, define_xml_str
 from utils.utils import get_rebase_backing_file_cmds, add_spec_in_volume, get_hostname_in_lower_case, DiskImageHelper, updateDescription, get_volume_snapshots, updateJsonRemoveLifecycle, addSnapshots, report_failure, addPowerStatusMessage, RotatingOperation, ExecuteException, string_switch, deleteLifecycleInJson
 from utils import logger
-from executor.utils.libvirt_util import get_volume_path
 
 class parser(ConfigParser.ConfigParser):  
     def __init__(self,defaults=None):  

4d8a2900d7ef57c4e60b51200e62141fc226ca6d
742362144 <742362144@qq.com>
2019-12-10 21:09:43 +0800

    delete lifecycle

--- executor/invoker.py
+++ executor/invoker.py
@@ -1535,7 +1535,7 @@ def write_result_to_server(group, version, namespace, plural, name, result=None,
             jsonDict['spec']['volume'] = data
         elif plural == PLURAL_VM_DISK_SNAPSHOT:
             if _isRevertDiskExternalSnapshot(the_cmd_key) or _isCreateDiskExternalSnapshot(the_cmd_key):
-                modify_disk(data['pool'], data['disk'], GROUP_VM_DISK, VERSION_VM_DISK, PLURAL_VM_DISK)
+                modify_disk(data['poolname'], data['disk'], GROUP_VM_DISK, VERSION_VM_DISK, PLURAL_VM_DISK)
             if _isCreateDiskExternalSnapshot(the_cmd_key):
                 jsonDict['spec']['volume'] = data
         elif plural == PLURAL_VM:

c74e84775d31fd021354a6038ee80d2ba201365d
wuyuewen <340629194@qq.com>
2020-03-16 10:52:59 +0800

    change thread to process

--- executor/virtlet_in_docker.py
+++ executor/virtlet_in_docker.py
@@ -8,6 +8,7 @@ Copyright (2019, ) Institute of Software, Chinese Academy of Sciences
  
 import os, sys, ConfigParser, traceback, time, socket
 from threading import Thread
+from multiprocessing import Process
 
 '''
 Import third party libs
@@ -52,17 +53,17 @@ def main():
     if os.path.exists(TOKEN):
         config.load_kube_config(config_file=TOKEN)
         try:
-            thread_1 = Thread(target=libvirt_event_handler)
-            thread_1.daemon = True
+            thread_1 = Process(target=get_libvirt_event_handler())
+#             thread_1.daemon = True
             thread_1.name = 'libvirt_event_handler'
             thread_1.start()
-            thread_2 = Thread(target=os_event_handler)
-            thread_2.daemon = True
+            thread_2 = Process(target=os_event_handler)
+#             thread_2.daemon = True
             thread_2.name = 'os_event_handler'
             thread_2.start()
 #             if not is_kubernetes_master():
-            thread_3 = Thread(target=host_cycler)
-            thread_3.daemon = True
+            thread_3 = Process(target=host_cycler)
+#             thread_3.daemon = True
             thread_3.name = 'host_cycler'
             thread_3.start()
             try:

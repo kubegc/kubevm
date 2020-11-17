bdd6f7b54c24a87869326b1d0db6c63fb91ac6b2
wuyuewen <340629194@qq.com>
2020-11-17 14:54:12 +0800

    fix

--- executor/libvirt_event_handler.py
+++ executor/libvirt_event_handler.py
@@ -1208,4 +1208,4 @@ if __name__ == "__main__":
         except Exception, e:
             config.load_kube_config(config_file=TOKEN)
             logger.error('Oops! ', exc_info=1)
-            break;
+            sys.exit(1)
